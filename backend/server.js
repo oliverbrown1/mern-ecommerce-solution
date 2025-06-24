eebd9ee4b55dff65c32b2308012e7937fa3b5679
Oliver Brown <olivertouki@gmail.com>
2025-06-24 22:13:49 +0100

    authentication middleware logic complete

--- backend/server.js
+++ backend/server.js
@@ -6,15 +6,19 @@ const port = 3001
 
 
 
-// middleware
+// middleware routes
+app.use(express.json());
 app.use("/test",loginRoutes);
 
+// initiate DB connection
 connectDB();
 
+// start message
 app.listen(port, () => {
     console.log(`server started on ${port}`);
 })
 
+// routes
 app.get("/help", (req,res) => {
     res.status(200).send("Hi");
 })
